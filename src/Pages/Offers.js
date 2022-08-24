08c7376fc6f43861c6e989f9fbeea98879923136
ronaknavadiya <ronaknavadiya123@gmail.com>
2022-08-24 12:55:04 -0400

    commit ronak explore page done

--- src/Pages/Offers.js
+++ src/Pages/Offers.js
@@ -1,9 +1,86 @@
-import React from 'react'
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { database } from "../firebase.config";
+import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
 
 const Offers = () => {
-  return (
-    <div>Offers</div>
-  )
-}
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default Offers
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const listingsRef = collection(database, "listings");
+
+        const listingQuery = query(
+          listingsRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        const querySnapshot = await getDocs(listingQuery);
+
+        let listings = [];
+
+        querySnapshot.forEach((doc) => {
+          return listings.push({ id: doc.id, data: doc.data() });
+        });
+
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Couldn't fetch listings");
+      }
+    };
+
+    fetchListings();
+  }, []);
+
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">
+          Offers
+        </p>
+      </header>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => {
+                return (
+                  <ListingItem
+                    key={listing.id}
+                    id={listing.id}
+                    listing={listing.data}
+                  />
+                );
+              })}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No current offers, Please explore from home page..</p>
+      )}
+    </div>
+  );
+};
+
+export default Offers;
